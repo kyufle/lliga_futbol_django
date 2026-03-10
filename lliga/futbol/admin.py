b2b34fd01bbc026f24d053e7a6d51fcfbaeb5e61
Ruth <rromerocarretero.cf@iesesteveterradas.cat>
2026-03-10 17:27:40 +0100

    You can modify the match, filter by team and player

--- lliga/futbol/admin.py
+++ lliga/futbol/admin.py
@@ -7,11 +7,26 @@ admin.site.register(Jugador)
 
 class EventInline(admin.TabularInline):
     model = Event
-    extra = 2
+    fields = ["temps","tipus","jugador","equip"]
+    ordering = ("temps",)
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # filtrem els jugadors i només deixem els que siguin d'algun dels 2 equips (local o visitant)
+        if db_field.name == "jugador":
+            partit_id = request.resolver_match.kwargs['object_id']
+            partit = Partit.objects.get(id=partit_id)
+            # fem un queryset per cada equip
+            jugadors_locals = partit.local.jugadors.all()
+            jugadors_visitants = partit.visitant.jugadors.all()
+            # fusionem els 2 querysets amb l'operador | (= union en BD)
+            kwargs["queryset"] = jugadors_locals | jugadors_visitants
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+    
 
 class PartitAdmin(admin.ModelAdmin):
+    search_fields = ['local__nom', 'visitant__nom', 'lliga__nom']
+    list_display = ('lliga', 'local', 'visitant', 'gols_local', 'gols_visitant')
     inlines = [EventInline]
-    list_display = ('__str__', 'data', 'lliga')
-    list_filter = ('lliga', 'data')
 
 admin.site.register(Partit, PartitAdmin)
